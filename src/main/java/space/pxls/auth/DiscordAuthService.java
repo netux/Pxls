4a11f0e6cea24a73e009c435e74f8e7f4fc87f9e
Martín (Netux) Rodríguez <send.email+git@netux.site>
2020-08-31 17:21:38 -0300

    Add an option to restrict Discord oauth to members of certain servers.

--- src/main/java/space/pxls/auth/DiscordAuthService.java
+++ src/main/java/space/pxls/auth/DiscordAuthService.java
@@ -4,9 +4,11 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+
 import org.json.JSONObject;
 import space.pxls.App;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class DiscordAuthService extends AuthService {
@@ -15,7 +17,8 @@ public class DiscordAuthService extends AuthService {
     }
 
     public String getRedirectUrl(String state) {
-        return "https://discord.com/api/oauth2/authorize?client_id=" + App.getConfig().getString("oauth.discord.key") + "&response_type=code&redirect_uri=" + getCallbackUrl() + "&duration=temporary&scope=identify&state=" + state;
+        String scope = "identify" + (App.getConfig().hasPath("oauth.discord.memberOfGuilds") ? "+guilds" : "");
+        return "https://discord.com/api/oauth2/authorize?client_id=" + App.getConfig().getString("oauth.discord.key") + "&response_type=code&redirect_uri=" + getCallbackUrl() + "&duration=temporary&scope=" + scope + "&state=" + state;
     }
 
     public String getToken(String code) throws UnirestException {
@@ -41,21 +44,47 @@ public class DiscordAuthService extends AuthService {
                 .header("Authorization", "Bearer " + token)
                 .header("User-Agent", "pxls.space")
                 .asJson();
-        JSONObject json = me.getBody().getObject();
-        if (json.has("error")) {
+        JSONObject meJSON = me.getBody().getObject();
+        if (meJSON.has("error")) {
             return null;
-        } else {
-            long id = json.getLong("id");
-            long signupTimeMillis = (id >> 22) + 1420070400000L;
+        }
+
+        String id = meJSON.getString("id");
+
+        if (App.getConfig().hasPath("oauth.discord.minAge")) {
+            long idLong = Long.parseLong(id);
+            long signupTimeMillis = (idLong >> 22) + 1420070400000L;
             long ageMillis = System.currentTimeMillis() - signupTimeMillis;
 
             long minAgeMillis = App.getConfig().getDuration("oauth.discord.minAge", TimeUnit.MILLISECONDS);
             if (ageMillis < minAgeMillis){
-                long days = minAgeMillis / 86400 / 1000;
                 throw new InvalidAccountException("Account too young");
             }
-            return json.getString("id");
         }
+
+        if (App.getConfig().hasPath("oauth.discord.memberOfGuilds")) {
+            final List<Long> allowedGuilds = App.getConfig().getLongList("oauth.discord.memberOfGuilds");
+            HttpResponse<JsonNode> guilds = Unirest.get("https://discord.com/api/users/@me/guilds")
+                    .header("Authorization", "Bearer " + token)
+                    .header("User-Agent", "pxls.space")
+                    .asJson();
+            if (!guilds.getBody().isArray()) {
+                return null;
+            }
+
+            boolean isAllowed = false;
+            for (Object obj : guilds.getBody().getArray()) {
+                if (obj instanceof JSONObject && allowedGuilds.contains(((JSONObject) obj).getLong("id"))) {
+                    isAllowed = true;
+                    break;
+                }
+            }
+            if (!isAllowed) {
+                throw new InvalidAccountException("Account isn't in any allowed server.");
+            }
+        }
+
+        return meJSON.getString("id");
     }
 
     public String getName() {
